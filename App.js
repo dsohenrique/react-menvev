f0872c72abbfba50e42eabd3762b4998874be804
dsohenrique <dsohenrique@gmail.com>
2020-02-03 18:53:12 -0300

    1 file modified

--- App.js
+++ App.js
@@ -4,30 +4,35 @@ import api from "./service/api";
 
 class App extends Component {
 
-  state = {
-    personagens = [],
+constructor(props) {
+    super(props);
+    this.state = {
+      personagens: [],
+    }
   }
 
   async componentDidMount() {
     await api.get("people").then(
       response => {
         console.log(response.data.results);
+        this.setState({personagens: response.data.results});
       }
     )
   };
   
 render(){
+  const { personagens } = this.state;
   return (
     <div id="app">
       <main>
         <ul>
-         /* {personagens
+        {personagens
             .sort((a, b) => a.name > b.name == 1)
             .map((personagem, index) => (
               <Personagem key={index}
                personagem={personagem} 
                />
-            ))}*/
+            ))}
         </ul>
       </main>
     </div>
@@ -36,3 +41,5 @@ render(){
 }
 
 export default App;
+  /*
+            */
